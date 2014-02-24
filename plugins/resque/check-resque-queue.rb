c802a65e88af08668c8b0b4b439adaf598228602
MrPink <w.pink@tape.tv>
2014-02-24 15:22:46 +0100

    changes to resque check

--- plugins/resque/check-resque-queue.rb
+++ plugins/resque/check-resque-queue.rb
@@ -1,9 +1,11 @@
+#!/usr/bin/ruby 
+
 require 'sensu-plugin/check/cli'
 require 'resque'
 require 'socket'
 require 'resque/failure/redis'
 
-class Resque_queue_length  < Sensu::Plugin::Check::CLI
+class Resque_queue_length < Sensu::Plugin::Check::CLI
 
   option :hostname,
     :short => "-h HOSTNAME",
@@ -26,15 +28,8 @@ class Resque_queue_length  < Sensu::Plugin::Check::CLI
     :short => '-w WARNING_THRESHOLD',
     :long => '--warning WARNING_THRESHOLD'
 
-def run
-
-redis = Redis.new(:host => config[:hostname], :port => config[:port])
+redis = Redis.new(:host => config[:hostname])
 Resque.redis = redis
-
-#count = Resque::Failure::Redis.count
-#info = Resque.info
-
-Resque.queues.each do |v|
 
 def critical_length
   unknown "No critical threshold specified" unless config[:critical_threshold]
@@ -42,28 +37,24 @@ def critical_length
 end
 
 def warning_length
-  unknown "No critical threshold specified" unless config[:warning_threshold]
-  config[:warning_threshold].to_i
+   unknown "No critical threshold specified" unless config[:warning_threshold]
+   config[:warning_threshold].to_i
 end
 
-def current_queue_size
-  Resque.size("#{v}")
-end
-
-def monitoring_queue
-  print "Monitoring #{v} queue\n"
-end
-
-  if "#{current_queue_size}" >= "#{critical_length}"
-    puts "Critical - Queue currently has #{current_queue_size} items\n"
-    exit 2
-  elsif "#{current_queue_size}" >= "#{warning_length}"
-    puts "Warning - the queue is currently at #{current_queue_size}\n"
-    exit 1
-  else 
-    puts "OK - The queue has #{current_queue_size} items"
-    exit 0
+  sz = Resque.size(config[:queue])
+  if sz >= critical_length
+     crit << "Critical the queue #{v} has currently #{sz}"
+     elsif sz >= warning_length
+     warn << "Warning the queue #{v} has currently #{sz}"
+     else
+     puts "#{v} OK"
   end
-end
-end
+
+  def run
+    critical usage_summary unless @crit.empty?
+    warning usage_summary unless @warn.empty?
+    ok "Queue's are fine"
+  end
+ end
+
 end
